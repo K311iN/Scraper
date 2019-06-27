230d0285e58ecaa2e6da18636490d0d21db4d520
Kellin Baldenegro <kellinjoshua@gmail.com>
2019-06-26 23:31:31 -0700

    complete server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+let bodyParser = require("body-parser");
+let mongoose = require("mongoose");
+let logger = require("morgan");
+
+let express = require("express");
+let app = express();
+
+app.use(logger("dev"));
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+app.use(express.static(process.cwd() + "/public"));
+
+let exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+mongoose.connect("mongodb://localhost/scraper");
+let db = mongoose.connection;
+
+db.model("error", console.error.bind(console, "connection error"));
+db.once("open", function () {
+    console.log("Connected to Mongoose!");
+});
+
+
+let port = process.env.PORT = 3000;
+app.listen(port, function () {
+    console.log("listening on PORT " + port);
+});
